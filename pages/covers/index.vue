3db2efc868f4c658326a00d499e199c06f0f825b
Luiz Barreto <llbarre@hotmail.com>
2024-11-26 22:11:16 -0600

    feat: new landing page for covers

--- pages/covers/index.vue
+++ pages/covers/index.vue
@@ -1,14 +1,19 @@
-<template>
-  <div>
-    <h1>Covers</h1>
-  </div>
-</template>
-
 <script lang="ts" setup>
 import type { Cover } from "@/types/covers"
 
-const { data: featured, status } = await useAsyncData<Cover[] | null>(
+const { data: allCovers, status } = await useAsyncData<Cover[] | null>(
   'allCovers',
-  () => $fetch('/api/covers?featured=true'),
+  () => $fetch('/api/covers'),
 )
 </script>
+
+<template>
+  <UContainer class="py-8 ">
+    <UPageHero title="Covers" align="center" />
+    <ul class="grid grid-cols-2 place-content-center md:grid-cols-4 gap-8">
+      <ULink :to="`/covers/${cover.id}`" class="group" v-for="cover in allCovers" :key="cover.id">
+        <AlbumCover :cover="cover" />
+      </ULink>
+    </ul>
+  </UContainer>
+</template>
