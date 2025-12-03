f3b49aa3891d5b614ed87ea6678e3958fb1b4f0a
Mike Frost <mike@slfemp.com>
2025-12-03 13:48:26 -0600

    moved site to new location

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -23,7 +23,7 @@ export default defineNuxtConfig({
     componentIslands: true,
   },
   site: {
-    url: "https://slfemp.github.io/slfemp/", // Updated to reflect GitHub Pages URL
+    url: "https://slfemp.netlify.app", // Updated to match actual Netlify deployment
     name: "SLFEMP",
     description: "Houston Legend",
     defaultLocale: "en", // not needed if you have @nuxtjs/i18n installed
@@ -35,6 +35,8 @@ export default defineNuxtConfig({
     ],
   },
   image: {
+    provider: 'none', // Disable image optimization
+    domains: ['wuspgfcmadudsgmijbtk.supabase.co'],
   },
   // https://html-validator.nuxtjs.org/
   htmlValidator: {
